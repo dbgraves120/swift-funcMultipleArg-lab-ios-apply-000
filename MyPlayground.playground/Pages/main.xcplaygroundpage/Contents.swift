72ccd84850191656be1b079fb19413f46329cee0
David Graves <dbgraves120@gmail.com>
2016-12-04 05:55:44 -0500

    Completed

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,28 +13,26 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
-
-
+func description(nameOfContinet: String, numCountries: Int)
+{
+    print("\(nameOfContinet) is a continet which contains \(numCountries)")
+}
 
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
  */
-func greeting(name: String, greeting: String) {
+func greeting(name: String, greeting: String)
+{
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
-
-
-/*: question3
- ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
- */
-func daysInMonth(month: String, days: String) {
-    print("There are \(days) in \(month)")
+func daysInMonth(month: String, days: Int)
+{
+    print("There are \(days) days in \(month)")
 }
 
 daysInMonth(month: "November", days: 30)
@@ -45,11 +43,13 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func beverage(numBottles: Int, beverageName: String, beverageLocation: String)
+{
+    print("\(numBottles) bottles of \(beverageName) on the \(beverageLocation) wall")
+}
 
 
-
-
+beverage(numBottles: 12, beverageName: "Budwiser", beverageLocation: "South")
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
  */
